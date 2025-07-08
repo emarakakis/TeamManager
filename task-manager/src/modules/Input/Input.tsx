fefc688d36c662615eba4040ff1c5719587d934c
emarakakis <eftihismarak@gmail.com>
2025-07-08 16:08:34 +0300

    bug: Fixed URL and Input value corelation

--- task-manager/src/modules/Input/Input.tsx
+++ task-manager/src/modules/Input/Input.tsx
@@ -1,12 +1,13 @@
-import { useRef,ChangeEvent } from "react"
+import { useRef,ChangeEvent, useState } from "react"
 import { TextField } from "@mui/material"
 import { useQueryState } from "@/app/hooks/query-state-hook"
 export default function Input(){
     const timeoutTime = useRef<number | null>(null)
     const [searchEmployee, setSearchEmployee] = useQueryState('employee')
+    const [inputValue, setInputValue] = useState<string>(searchEmployee ?? "")
     function handleChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
         const value = event.target.value;
-
+        setInputValue(inputValue)
         if (timeoutTime.current)
             clearTimeout(timeoutTime.current)
 
@@ -19,5 +20,5 @@ export default function Input(){
         }, 500)
     }
 
-    return <TextField type="text" onChange={handleChange}/>
+    return <TextField type="text" value={inputValue} onChange={handleChange}/>
 }
