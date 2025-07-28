5554cf4e544402728b7e78215046836c92be685f
emarakakis <eftihismarak@gmail.com>
2025-07-28 12:51:58 +0300

    fix: Fully Functional EmployeeJob Change

--- task-manager/src/app/api/job/route.ts
+++ task-manager/src/app/api/job/route.ts
@@ -2,56 +2,68 @@ import { NextResponse } from "next/server";
 import { db, jobTable } from "../../../../db";
 import { eq } from "drizzle-orm";
 
-export async function POST(req:Request) {
-    try{
-        const job = await req.json()
-        const result = await db.insert(jobTable).values(job)
+export async function POST(req: Request) {
+  try {
+    const job = await req.json();
+    const result = await db.insert(jobTable).values(job);
 
-        return NextResponse.json({success: true})
-    } catch (error) {
-        throw error
-    }
+    return NextResponse.json({ success: true, id: result.lastInsertRowid });
+  } catch (error) {
+    throw error;
+  }
 }
 
-export async function DELETE(req:Request) {
-    try{
-        const url = new URL(req.url)
-        const id = url.searchParams.get('id')
-        console.log(id)
+export async function DELETE(req: Request) {
+  try {
+    const url = new URL(req.url);
+    const id = url.searchParams.get("id");
+    console.log(id);
 
-        const result = await db.delete(jobTable).where(eq(jobTable.id, Number(id)))
+    const result = await db.delete(jobTable).where(eq(jobTable.id, Number(id)));
 
-        return NextResponse.json({success:true})
-
-    } catch(error) {
-        throw error
-    }
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    throw error;
+  }
 }
 
-export async function GET(req:Request){
-    try{
-        const url = new URL(req.url)
-        const id = url.searchParams.get('id')
+export async function GET(req: Request) {
+  try {
+    const url = new URL(req.url);
+    const id = url.searchParams.get("id");
 
-        const result = await db.select().from(jobTable).where(eq(jobTable.id, Number(id)))
+    const result = await db
+      .select()
+      .from(jobTable)
+      .where(eq(jobTable.id, Number(id)));
 
-        if (result.length > 0){
-            return NextResponse.json({success:true, data: result[0]})
-        } else {
-            return NextResponse.json({success:false})
-        }
-    } catch (error) {
-        throw error
+    if (result.length > 0) {
+      return NextResponse.json({ success: true, data: result[0] });
+    } else {
+      return NextResponse.json({ success: false });
     }
+  } catch (error) {
+    throw error;
+  }
 }
 
-export async function PUT(req: Request){
-    try {
-        const job = await req.json()
-
-        const result = await db.update(jobTable).set(job).where(eq(jobTable.id, Number(job.id)))
-        return NextResponse.json({success:true})
-    } catch (error) {
-        throw error
+export async function PUT(req: Request) {
+  try {
+    const job = await req.json();
+    if (job.type === "assignAction") {
+      const result = await db
+        .update(jobTable)
+        .set({ assigned: job.assigned })
+        .where(eq(jobTable.id, Number(job.id)));
+      return NextResponse.json({ success: true });
+    } else {
+      const result = await db
+        .update(jobTable)
+        .set(job)
+        .where(eq(jobTable.id, Number(job.id)));
+      return NextResponse.json({ success: true });
     }
+  } catch (error) {
+    throw error;
+  }
 }
--- task-manager/src/serverFunctions/putEmployeeJob.ts
+++ task-manager/src/serverFunctions/putEmployeeJob.ts
@@ -1,6 +1,8 @@
 import { EmployeeJobId, EmployeeJobReturn } from "@/types/EmployeeJob";
 import axios from "axios";
 import postFieldJob from "./postFieldJob";
+import getJob from "./getJob";
+import postJob from "./postJob";
 
 export default async function putEmployeeJob(info: {
   employeeJobId: number;
@@ -8,35 +10,24 @@ export default async function putEmployeeJob(info: {
   itemId: number;
 }) {
   const { employeeJobId, type, itemId } = { ...info };
+  console.log("Wtf");
   const employeeJob = await axios.get<{
     data: { id: number } & EmployeeJobId;
     success: boolean;
   }>(`/api/employeeJob?id=${employeeJobId}`);
   const employeeJobData = employeeJob.data.data;
-  console.log(employeeJobData);
+
   if (type === "employee") {
-  } else if (type === "field") {
-    let unAssignCurrent = await axios.put(`/api/fieldJob`, {
-      type: "assignAction",
-      assigned: 0,
-      jobId: employeeJobData.jobId,
-      fieldId: employeeJobData.fieldId,
-    });
-
-    if (!unAssignCurrent.data.success)
-      throw new Error("Something went wrong when Un-Assigning Field-Job");
-
-    let createNewFieldJob = await postFieldJob({
-      jobId: employeeJobData.jobId.toString(),
-      fieldId: itemId.toString(),
-    });
-
-    let updateFieldJob = await axios.put(`/api/fieldJob`, {
-      type: "assignAction",
-      assigned: 1,
-      jobId: employeeJobData.jobId,
-      fieldId: itemId,
-    });
+    await updateEmployee(employeeJobData.employeeId, info.itemId);
+  } else {
+    await updateFieldJob(
+      type,
+      {
+        jobId: employeeJobData.jobId,
+        fieldId: employeeJobData.fieldId,
+      },
+      itemId
+    );
   }
 
   const result = await axios.put(`/api/employeeJob`, info);
@@ -48,4 +39,97 @@ export default async function putEmployeeJob(info: {
   }
 
   console.log("Ended Put");
+}
+
+async function updateEmployee(oldId: number, newId: number) {
+  await axios.put(`/api/employee`, {
+    type: "assignAction",
+    assigned: 0,
+    id: oldId,
+  });
+
+  await axios.put(`/api/employee`, {
+    type: "assignAction",
+    assigned: 1,
+    id: newId,
+  });
+}
+
+async function updateFieldJob(
+  type: string,
+  fieldJobId: { jobId: number; fieldId: number },
+  itemId: number
+) {
+  const unAssignCurrent = await axios.put(`/api/fieldJob`, {
+    type: "assignAction",
+    assigned: 0,
+    jobId: fieldJobId.jobId,
+    fieldId: fieldJobId.fieldId,
+  });
+
+  if (!unAssignCurrent.data.success)
+    throw new Error("Something went wrong when Un-Assigning Field-Job");
+
+  const [fieldId, jobId] =
+    type === "field"
+      ? await updateField(fieldJobId, itemId)
+      : await updateJob(fieldJobId, itemId);
+
+  let updateFieldJob = await axios.put(`/api/fieldJob`, {
+    type: "assignAction",
+    assigned: 1,
+    jobId: jobId,
+    fieldId: fieldId,
+  });
+}
+
+async function updateField(
+  oldId: { jobId: number; fieldId: number },
+  newId: number
+) {
+  const [fieldId, jobId] = await getFieldJobId(oldId.jobId, newId);
+  return [fieldId, jobId];
+}
+
+async function updateJob(
+  oldId: { jobId: number; fieldId: number },
+  newId: number
+) {
+  let unAssignCurrent = await axios.put(`/api/job`, {
+    type: "assignAction",
+    assigned: 0,
+    id: oldId.jobId,
+  });
+
+  const [fieldId, jobId] = await getFieldJobId(newId, oldId.fieldId);
+
+  let assignCurrent = await axios.put(`/api/job`, {
+    type: "assignAction",
+    assigned: 1,
+    id: jobId,
+  });
+
+  return [fieldId, jobId];
+}
+
+async function getFieldJobId(jobId: number, fieldId: number) {
+  const existingFieldJob = await axios.get(
+    `/api/fieldJob?fieldId=${fieldId}&jobId=${jobId}`
+  );
+  const existingData = existingFieldJob.data;
+  if (!existingData.success) {
+    await postFieldJob({
+      jobId: jobId.toString(),
+      fieldId: fieldId.toString(),
+    });
+    return [jobId, fieldId];
+  } else {
+    const existingFieldJob = existingData.data;
+    if (!existingFieldJob.assigned) {
+      console.log("Unassigned FieldJob Exists!");
+      return [jobId, fieldId];
+    } else {
+      throw new Error("Exists Already! No Implementation");
+    }
+  }
 }
